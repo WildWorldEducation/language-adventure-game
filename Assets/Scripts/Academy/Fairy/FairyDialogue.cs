dd4b9ee78bcf1cc02d046b8cfd969c4647452b79
RussiSunni <jonathandyason@hotmail.com>
2022-07-25 22:29:53 +0200

    update

---
+++ Assets/Scripts/Academy/Fairy/FairyDialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FairyDialogue : MonoBehaviour
+{
+    [SerializeField]
+    private Image _fairyDialoguePanel;
+   
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        _fairyDialoguePanel.GetComponent<CanvasGroup>().alpha = 1;
+        _fairyDialoguePanel.GetComponent<CanvasGroup>().interactable = true;        
+    }
+
+    public void CloseDialoguePanel()
+    {
+        _fairyDialoguePanel.GetComponent<CanvasGroup>().alpha = 0;
+        _fairyDialoguePanel.GetComponent<CanvasGroup>().interactable = false;
+    }
+}
